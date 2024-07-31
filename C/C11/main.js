c745d617f1c5145d84acadfbf216c3aaedbad219
SodiumN1trate <renarsgausins21@gmail.com>
2024-07-31 20:22:58 +0300

    fix: fixed C11

--- C/C11/main.js
+++ C/C11/main.js
@@ -1,36 +1,38 @@
-const buttons = document.querySelectorAll('.btn')
+const buttons = document.querySelectorAll('.thumbs-up ,.thumbs-down')
 
-let comments = []
-
-const like = (commentId, liked) => {
-    let removeHighlight = false
-    let commentContainer = document.getElementById(commentId)
-    commentContainer.childNodes[3].childNodes[1].style.backgroundColor = '#f8f9fa'
-    commentContainer.childNodes[3].childNodes[3].style.backgroundColor = '#f8f9fa'
-
-    let existingComment = comments.filter((comment) => comment.id === commentId)
-    if (existingComment.length === 1 && existingComment[0].liked === liked) {
-        comments = comments.filter((comment) => comment.id !== commentId)
-        removeHighlight = true
-    } else {
-        comments = comments.filter((comment) => comment.id !== commentId)
-        comments.push({
-            id: commentId,
-            liked: liked
-        })
-    }
-
-    if (!removeHighlight && liked) {
-        let button = commentContainer.childNodes[3].childNodes[1]
-        button.style.backgroundColor = 'gray'
-    } else if (!removeHighlight && liked === false) {
-        let button = commentContainer.childNodes[3].childNodes[3]
-        button.style.backgroundColor = 'gray'
-    }
+const clearClasses = (comment) => {
+    const buttons = comment.querySelectorAll('.thumbs-up ,.thumbs-down')
+    buttons[0].classList.remove('highlight')
+    buttons[1].classList.remove('highlight')
 }
 
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let button = e.target
+        if (e.target.parentNode.classList.contains('thumbs-up') || e.target.parentNode.classList.contains('thumbs-down')) {
+            button = button.parentNode
+        }
+        let comment = button.parentNode.parentNode
+        if(button.classList.contains('highlight')) {
+            button.classList.remove('highlight')
+        } else {
+            clearClasses(comment)
+            button.classList.add('highlight')
+        }
+    })
+})
+
 const summary = () => {
-    const likes = comments.filter((comment) => comment.liked === true)
-    const dislikes = comments.filter((comment) => comment.liked === false)
-    alert(`Thumbs up: ${likes.length} \n Thumbs down: ${dislikes.length}`)
+    let thumbsUp = 0;
+    let thumbsDown = 0;
+    document.querySelectorAll('.highlight').forEach((button) => {
+        if(button.classList.contains('thumbs-up')) {
+            thumbsUp += 1
+        }
+        if(button.classList.contains('thumbs-down')) {
+            thumbsDown += 1
+        }
+    })
+
+    alert(`Thumbs-up: ${thumbsUp} \n Thumbs-down: ${thumbsDown}`)
 }
